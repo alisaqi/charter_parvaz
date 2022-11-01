9522cc5f7d4ad58d59051f9ea2a38a407b4448be
Ali Saqi <alisaghi0931@gmail.com>
2022-11-01 16:12:07 +0330

    اضافه کردن تعداد شهر ها

--- config.py
+++ config.py
@@ -336,7 +336,7 @@ cities = {
             'internationalId': False,
         }
     },
-    'Gorgan' : {
+    'گرگان' : {
         'id': 22,
         'title': 'Gorgan',
         'name': 'گرگان',
@@ -408,7 +408,7 @@ cities = {
             'internationalId': 5,
         }
     },
-    'Abadan' : {
+    'آبادان' : {
         'id': 26,
         'title': 'Abadan',
         'name': 'آبادان',
@@ -624,7 +624,7 @@ cities = {
             'internationalId': False,
         }
     },
-    'Kermanshah' : {
+    'کرمانشاه' : {
         'id': 38,
         'title': 'Kermanshah',
         'name': 'کرمانشاه',
@@ -660,10 +660,10 @@ cities = {
             'internationalId': 5,
         }
     },
-    'Moscow_Vnukovo': {
+    'مسکو (ونوکووا)': {
         'id': 40,
         'title': 'Moscow Vnukovo',
-        'name': 'مسکو (ونوکوفو)',
+        'name': 'مسکو (ونوکووا)',
         'airport': {
             'en': 'Moscow Vnukovo Airport',
             'fa': 'فرودگاه ونوکوفو مسکو',
@@ -678,37 +678,546 @@ cities = {
             'internationalId': 5,
         }
     },
-    # 41: 'Dezful',
-    # 42: 'DELHI',
-    # 43: 'Alanya',
-    # 44: 'Khoramabad',
-    # 45: 'Bodrum_Kusadasi',
-    # 46: 'Bangkok',
-    # 47: 'Shahrekord',
-    # 48: 'Sari',
-    # 49: 'Varna',
-    # 50: 'Zabol',
-    # 51: 'Lahore',
-    # 52: 'Mumbai',
-    # 53: 'Antalya_Isparta',
-    # 54: 'Baghdad',
-    # 55: 'Tabas',
-    # 56: 'Bam',
-    # 57: 'Beirut',
-    # 58: 'Gheshm',
-    # 59: 'Ramsar',
-    # 60: 'Sabzevar',
-    # 61: 'Frankfurt',
-    # 62: 'BandarLenge',
-    # 63: 'Sanandaj',
-    # 64: 'Lar',
-    # 65: 'Karachi',
-    # 66: 'Yasooj',
-    # 67: 'Hamburg',
-    # 68: 'Tashkent',
-    # 69: 'Syrjan',
-    # 70: 'Maragheh',
-    # 71: 'Dushanbe',
+    'دزفول' : {
+        'id': 41,
+        'title': 'Dezful',
+        'name': 'دزفول',
+        'airport': {
+            'en': 'Dezful Airport',
+            'fa': 'فرودگاه دزفول',
+        },
+        'airportCode': 'DEF',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'دهلی' : {
+        'id': 42,
+        'title': 'DELHI',
+        'name': 'دهلی',
+        'airport': {
+            'en': 'Indira Gandhi International Airport',
+            'fa': 'فرودگاه بین‌المللی اندیرا گاندی',
+        },
+        'airportCode': 'DEL',
+        'timezone': 'Asia/Kolkata',
+        'timezoneOffsetStr': '+05:30',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'آلانیا' : {
+        'id': 43,
+        'title': 'Alanya',
+        'name': 'آلانیا',
+        'airport': {
+            'en': 'Alanya Gazipasa Airport',
+            'fa': 'فرودگاه آلانیا گازی پاسا',
+        },
+        'airportCode': 'GZP',
+        'timezone': 'Europe/Istanbul',
+        'timezoneOffsetStr': '+03:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'خرم آباد' : {
+        'id': 44,
+        'title': 'Khoramabad',
+        'name': 'خرم آباد',
+        'airport': {
+            'en': 'Khoramabad Airport',
+            'fa': 'فرودگاه خرم آباد',
+        },
+        'airportCode': 'KHD',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'کوش آداسی' : {
+        'id': 45,
+        'title': 'Bodrum_Kusadasi',
+        'name': 'کوش آداسی',
+        'airport': {
+            'en': 'Kusadasi Airport',
+            'fa': 'فرودگاه بدروم کوشاداسی',
+        },
+        'airportCode': 'BJV',
+        'timezone': 'Europe/Istanbul',
+        'timezoneOffsetStr': '+03:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'بانکوک (تایلند)' : {
+        'id': 46,
+        'title': 'Bangkok',
+        'name': 'بانکوک (تایلند)',
+        'airport': {
+            'en': 'Suvarnabhumi Airport',
+            'fa': 'فرودگاه سوارنابومی',
+        },
+        'airportCode': 'BKK',
+        'timezone': 'Asia/Bangkok',
+        'timezoneOffsetStr': '+07:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+   'شهر کرد' : {
+        'id': 47,
+        'title': 'Shahrekord',
+        'name': 'شهر کرد',
+        'airport': {
+            'en': 'Shahrekord International Airport',
+            'fa': 'فرودگاه بین المللی شهرکرد',
+        },
+        'airportCode': 'CQD',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+   },
+    'ساری' : {
+        'id': 48,
+        'title': 'Sari',
+        'name': 'ساری',
+        'airport': {
+            'en': 'Sari International Airport',
+            'fa': 'فرودگاه بین المللی ساری',
+        },
+        'airportCode': 'RAS',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'وارنا (بلغارستان)' : {
+        'id': 49,
+        'title': 'Varna',
+        'name': 'وارنا (بلغارستان)',
+        'airport': {
+            'en': 'Varna Airport',
+            'fa': 'فرودگاه وارنا',
+        },
+        'airportCode': 'VAR',
+        'timezone': 'Europe/Sofia',
+        'timezoneOffsetStr': '+03:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'زابل' : {
+        'id': 50,
+        'title': 'Zabol',
+        'name': 'زابل',
+        'airport': {
+            'en': 'Zabol Airport',
+            'fa': 'فرودگاه زابل',
+        },
+        'airportCode': 'ACZ',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'لاهور (پاکستان)' : {
+        'id': 51,
+        'title': 'Lahore',
+        'name': 'لاهور (پاکستان)',
+        'airport': {
+            'en': 'Lahore Airport',
+            'fa': 'فرودگاه لاهور',
+        },
+        'airportCode': 'LHE',
+        'timezone': 'Asia/Karachi',
+        'timezoneOffsetStr': '+05:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'بمبئی (هند)' : {
+        'id': 52,
+        'title': 'Mumbai',
+        'name': 'بمبئی (هند)',
+        'airport': {
+            'en': 'Chhatrapati Shivaji International Airport',
+            'fa': 'فرودگاه بین المللی چاترپاتی شیواجی',
+        },
+        'airportCode': 'BOM',
+        'timezone': 'Asia/Kolkata',
+        'timezoneOffsetStr': '+05:30',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'آنتالیا (ترکیه - اسپارتا)' : {
+        'id': 53,
+        'title': 'Antalya_Isparta',
+        'name': 'آنتالیا (ترکیه - اسپارتا)',
+        'airport': {
+            'en': 'Antalya Airport',
+            'fa': 'فرودگاه آنتالیا',
+        },
+        'airportCode': 'AYT',
+        'timezone': 'Europe/Istanbul',
+        'timezoneOffsetStr': '+03:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'بغداد (عراق)' : {
+        'id': 54,
+        'title': 'Baghdad',
+        'name': 'بغداد (عراق)',
+        'airport': {
+            'en': 'Baghdad International Airport',
+            'fa': 'فرودگاه بین المللی بغداد',
+        },
+        'airportCode': 'BGW',
+        'timezone': 'Asia/Baghdad',
+        'timezoneOffsetStr': '+03:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'طبس' : {
+        'id': 55,
+        'title': 'Tabas',
+        'name': 'طبس',
+        'airport': {
+            'en': 'Tabas Airport',
+            'fa': 'فرودگاه طبس',
+        },
+        'airportCode': 'TCX',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'بم' : {
+        'id': 56,
+        'title': 'Bam',
+        'name': 'بم',
+        'airport': {
+            'en': 'Bam Airport',
+            'fa': 'فرودگاه بم',
+        },
+        'airportCode': 'BXR',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'بیروت (لبنان)' : {
+        'id': 57,
+        'title': 'Beirut',
+        'name': 'بیروت (لبنان)',
+        'airport': {
+            'en': 'Beirut Rafic Hariri International Airport',
+            'fa': 'فرودگاه بین المللی رفیق حریری بیروت',
+        },
+        'airportCode': 'BEY',
+        'timezone': 'Asia/Beirut',
+        'timezoneOffsetStr': '+02:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'رامسر' : {
+        'id': 58,
+        'title': 'Ramsar',
+        'name': 'رامسر',
+        'airport': {
+            'en': 'Ramsar Airport',
+            'fa': 'فرودگاه رامسر',
+        },
+        'airportCode': 'RZR',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'سبزوار' : {
+        'id': 59,
+        'title': 'Sabzevar',
+        'name': 'سبزوار',
+        'airport': {
+            'en': 'Sabzevar Airport',
+            'fa': 'فرودگاه سبزوار',
+        },
+        'airportCode': 'AFZ',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'فرانکفورت' : {
+        'id': 60,
+        'title': 'Frankfurt',
+        'name': 'فرانکفورت',
+        'airport': {
+            'en': 'Frankfurt Airport',
+            'fa': 'فرودگاه فرانکفورت',
+        },
+        'airportCode': 'FRA',
+        'timezone': 'Europe/Berlin',
+        'timezoneOffsetStr': '+02:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'بندرلنگه' : {
+        'id': 61,
+        'title': 'BandarLenge',
+        'name': 'بندرلنگه',
+        'airport': {
+            'en': 'Bandar Lengeh International Airport',
+            'fa': 'فرودگاه بین المللی بندرلنگه',
+        },
+        'airportCode': 'BDH',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'سنندج' : {
+        'id': 62,
+        'title': 'Sanandaj',
+        'name': 'سنندج',
+        'airport': {
+            'en': 'Sanandaj Airport',
+            'fa': 'فرودگاه سنندج',
+        },
+        'airportCode': 'SDG',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'لار' : {
+        'id': 63,
+        'title': 'Lar',
+        'name': 'لار',
+        'airport': {
+            'en': 'Larestan International Airport',
+            'fa': 'فرودگاه بین المللی آیت‌اللهی لارستان',
+        },
+        'airportCode': 'LRR',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'کراچی (پاکستان)' : {
+        'id': 64,
+        'title': 'Karachi',
+        'name': 'کراچی (پاکستان)',
+        'airport': {
+            'en': 'Jinnah International Airport',
+            'fa': 'فرودگاه بین المللی جناح',
+        },
+        'airportCode': 'KHI',
+        'timezone': 'Asia/Karachi',
+        'timezoneOffsetStr': '+05:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'یاسوج' : {
+        'id': 65,
+        'title': 'Yasooj',
+        'name': 'یاسوج',
+        'airport': {
+            'en': 'Yasooj Airport',
+            'fa': 'فرودگاه شهدای یاسوج',
+        },
+        'airportCode': 'YES',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'هامبورگ (آلمان)' : {
+        'id': 66,
+        'title': 'Hamburg',
+        'name': 'هامبورگ (آلمان)',
+        'airport': {
+            'en': 'Hamburg Airport',
+            'fa': 'فرودگاه هامبورگ',
+        },
+        'airportCode': 'HAM',
+        'timezone': 'Europe/Berlin',
+        'timezoneOffsetStr': '+02:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'تاشکند (ازبکستان)' : {
+        'id': 67,
+        'title': 'Tashkent',
+        'name': 'تاشکند (ازبکستان)',
+        'airport': {
+            'en': 'Tashkent International Airport',
+            'fa': 'فرودگاه بین المللی تاشکند',
+        },
+        'airportCode': 'TAS',
+        'timezone': 'Asia/Tashkent',
+        'timezoneOffsetStr': '+05:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
+    'سیرجان' : {
+        'id': 68,
+        'title': 'Syrjan',
+        'name': 'سیرجان',
+        'airport': {
+            'en': 'Syrjan Airport',
+            'fa': 'فرودگاه سیرجان',
+        },
+        'airportCode': 'SYJ',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'مراغه' : {
+        'id': 69,
+        'title': 'Maragheh',
+        'name': 'مراغه',
+        'airport': {
+            'en': 'Maragheh Airport',
+            'fa': 'فرودگاه مراغه',
+        },
+        'airportCode': 'RJN',
+        'timezone': 'Asia/Tehran',
+        'timezoneOffsetStr': '+03:30',
+        'type': {
+            'domestic': True,
+            'domesticId': 10,
+            'international': False,
+            'internationalId': False,
+        }
+    },
+    'دوشنبه (تاجیکستان)' : {
+        'id': 70,
+        'title': 'Dushanbe',
+        'name': 'دوشنبه (تاجیکستان)',
+        'airport': {
+            'en': 'Dushanbe Airport',
+            'fa': 'فرودگاه دوشنبه',
+        },
+        'airportCode': 'DYU',
+        'timezone': 'Asia/Dushanbe',
+        'timezoneOffsetStr': '+05:00',
+        'type': {
+            'domestic': False,
+            'domesticId': False,
+            'international': True,
+            'internationalId': 5,
+        }
+    },
     # 72: 'Iranshar',
     # 73: 'Bojnourd',
     # 74: 'Kish',
